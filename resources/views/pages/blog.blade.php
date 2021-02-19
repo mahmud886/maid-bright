f1ff5bc6b4918dd83442d9059c47212cb9a33f45
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-02-19 23:14:04 +0600

    page elements created

---
+++ resources/views/pages/blog.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title')
+Maid Bangladesh | Blog
+@endsection
+
+@section('content')
+
+<h1>Blog Page</h1>
+
+@endsection
---
+++ resources/views/pages/gift-certificates.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title')
+Maid Bangladesh | Get Certificates
+@endsection
+
+@section('content')
+
+<h1>Get Certificates Page</h1>
+
+@endsection
