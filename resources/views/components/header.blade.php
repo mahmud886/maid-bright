a35041d1070e467df8762efe5ecc4c72a86ff7c7
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-02-20 14:02:10 +0600

    navbar added

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,69 @@
+<header>
+    <div class="container head-nav">
+        <div class="row">
+            <div class="col-2 col-lg-2 col-md-6 col-sm-12">
+                <img class="logo" src="{{ asset('/assets/images/maid-bright.svg') }}" alt="Maid Bright">
+            </div>
+            <div class="col-8 col-lg-8 col-md-6 col-sm-12">
+                <nav class="navbar navbar-expand-lg navbar-light">
+                    <div class="container-fluid">
+                        <a class="navbar-brand" href="#"></a>
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                        </button>
+                      <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav">
+                            <li class="nav-item dropdown">
+                                <a class="nav-link  dropdown-toggle" href="#" data-toggle="dropdown">  About US  </a>
+                                 <ul class="dropdown-menu">
+                                     <li><a class="dropdown-item" href="#"> Why Maid Bright </a></li>
+                                     <li><a class="dropdown-item" href="#"> Meet The Team </a></li>
+                                     <li><a class="dropdown-item" href="#"> Our Testimonials </a></li>
+                                     <li><a class="dropdown-item" href="#"> Our Videos </a></li>
+                                 </ul>
+                             </li>
+                             <li class="nav-item dropdown">
+                                <a class="nav-link  dropdown-toggle" href="#" data-toggle="dropdown">  Cleaning Services  </a>
+                                 <ul class="dropdown-menu">
+                                     <li><a class="dropdown-item" href="#"> Housekeeping Services </a></li>
+                                     <li><a class="dropdown-item" href="#"> Recurring Cleaning </a></li>
+                                     <li><a class="dropdown-item" href="#"> Moving Cleaning </a></li>
+                                     <li><a class="dropdown-item" href="#"> One-Time Cleaning </a></li>
+                                     <li><a class="dropdown-item" href="#"> Apartment Cleaning Services </a></li>
+                                     <li><a class="dropdown-item" href="#"> Special Cleanings </a></li>
+                                     <li><a class="dropdown-item" href="#"> Seasonal Cleaning and Family Gatherings </a></li>
+                                 </ul>
+                             </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Offers</a>
+                          </li>
+                          <li class="nav-item">
+                            <a class="nav-link" href="#">Get Certificates</a>
+                          </li>
+                          <li class="nav-item dropdown">
+                            <a class="nav-link  dropdown-toggle" href="#" data-toggle="dropdown">  Contact US  </a>
+                             <ul class="dropdown-menu">
+                                 <li><a class="dropdown-item" href="#"> Schedule Your Cleaning </a></li>
+                                 <li><a class="dropdown-item" href="#"> Request An Estimate </a></li>
+                                 <li><a class="dropdown-item" href="#"> Provide Feedback </a></li>
+                                 <li><a class="dropdown-item" href="#"> F.A.Q </a></li>
+                                 <li><a class="dropdown-item" href="#"> Careers </a></li>
+                             </ul>
+                         </li>
+                        </ul>
+                      </div>
+                    </div>
+                  </nav>
+            </div>
+            <div class="col-2 col-lg-2 col-md-12 col-sm-12 p-3 head-phone">
+                <a href="" class="nav-phone">
+                    <span class="button-text">+88019XXXXXXXX</span>
+                </a>
+            </div>
+        </div>
+    </div>
+    
+</header>
+<section class="head-section">
+    <a class="covid-text" href="">Maid Bright's Response to Your Coronavirus (COVID-19) Concerns</a>
+</section>
