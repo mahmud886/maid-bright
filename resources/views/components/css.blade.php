1c314991c8ebdff6edd8cdd43356868c38a3052d
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-02-20 11:00:49 +0600

    css files created

--- resources/views/components/css.blade.php
+++ resources/views/components/css.blade.php
@@ -24,5 +24,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 
 <!-- CUSTOM STYLESHEETS -->
+<link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}">
+<link rel="stylesheet" href="{{ asset('/assets/css/custom.css') }}">
 
 @stack('css')
