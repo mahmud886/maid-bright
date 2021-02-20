ede5b37413670472bb833f112213fc53e882862f
Snigdho <61132992+mahmud886@users.noreply.github.com>
2021-02-20 16:13:00 +0600

    banner fixed

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -12,11 +12,11 @@ Maid Bangladesh | Home
         <div class="container">
             <div class="banner-contents">
                 <div class="row">
-                    <div class="col-6 banner-overlay-color">
+                    <div class="col-4 banner-overlay-color">
                         <div class="banner-contents-div">
                             <h1>Let's Get Your Home <br> Cleaned! </h1>
                         </div>
-                        <p>Whether you require a one-time or recurring cleaning for your house or apartment, we’re available to personalize your service based on your needs. Make an appointment with a Maid Bright representative for a free in-home evaluation to discuss how we can customize your cleaning.</p>
+                        <p>Whether you require a one-time or recurring cleaning for your house or apartment, <br> we’re available to personalize your service based on your needs. Make an <br> appointment with a Maid Bright representative for a free in-home evaluation to <br> discuss how we can customize your cleaning.</p>
                         <div class="button-div">
                             <button  type="button" class="default-button1"><a href="http://"></a>Book Your Cleaning</button>
                         </div>
@@ -56,20 +56,20 @@ Maid Bangladesh | Home
             </div>
             <div class="col-3">
                 <div class="single-achivement">
-                    <h3>Award Winning Service</h3>
-                    <p>We a proven track record of results and for providing superior customer service. Our efforts are recognized by top consumer organizations such as Consumer’s Checkbook, Houzz, Yelp, Angie’s List and EBSCO Research.</p>
+                    <h3>Our Mission Statement</h3>
+                    <p>To offer specialized customer friendly cleaning services to residents in the Washington, DC area ensuring a positive experience every time.</p>
                 </div>
             </div>
             <div class="col-3">
                 <div class="single-achivement">
-                    <h3>Award Winning Service</h3>
-                    <p>We a proven track record of results and for providing superior customer service. Our efforts are recognized by top consumer organizations such as Consumer’s Checkbook, Houzz, Yelp, Angie’s List and EBSCO Research.</p>
+                    <h3>Family Owned Since 2004</h3>
+                    <p>Family owned and operated, we are a professional cleaning company specializing in a variety of services ensuring your home is cleaned to your satisfaction.</p>
                 </div>
             </div>
             <div class="col-3">
                 <div class="single-achivement">
-                    <h3>Award Winning Service</h3>
-                    <p>We a proven track record of results and for providing superior customer service. Our efforts are recognized by top consumer organizations such as Consumer’s Checkbook, Houzz, Yelp, Angie’s List and EBSCO Research.</p>
+                    <h3>Community Engagement</h3>
+                    <p>Maid Bright recently partnered with Children’s National Hospital by donating a portion of its proceeds towards raising pediatric care standards.</p>
                 </div>
             </div>
         </div>
