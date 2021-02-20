aa3855db5bf77a60cbe8a782e44061aaa05533b7
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-02-20 19:04:00 +0600

    offer page

--- resources/views/pages/offers.blade.php
+++ resources/views/pages/offers.blade.php
@@ -25,19 +25,45 @@ Maid Bangladesh | Offers
 </div>
 <div class="widget-div">
     <div class="row">
-        <div class="col-lg-6 col-md-12 col-sm-12 col-6">
+        <div class="col-lg-6 col-md-6 col-sm-12">
             <div class="widget-box">
                 <h3 class="widget-head">New Customer Special</h3>
-                <p>Receive 10% OFF Your First Cleaning, Move-In or Move-Out Cleaning. </p>
+                <p class="widget-p">Receive 10% OFF Your First Cleaning, Move-In or Move-Out Cleaning. </p>
+                <div class="button-div">
+                    <button  type="button" class="default-button2"><a href="http://"></a>Schedule A Free Estimate</button>
+                </div>
             </div>
 
         </div>
-    </div>
-    <div class="col-lg-6 col-md-12 col-sm-12 col-6">
-        <div class="widget-box">
-            <h3 class="widget-head">e-Gift Certificates</h3>
-            <p>For every e-gift certificate you buy, we will donate $10 to Children’s National Hospital.</p>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="widget-box">
+                <h3 class="widget-head">e-Gift Certificates</h3>
+                <p class="widget-p">For every e-gift certificate you buy, we will donate $10 to Children’s National Hospital.</p>
+                <div class="button-div">
+                    <button  type="button" class="default-button2"><a href="http://"></a>Purchage Gift certificate</button>
+                </div>
+            </div>
         </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="widget-box">
+                <h3 class="widget-head">$30 OFF Referral Discount</h3>
+                <p class="widget-p">Get $30 OFF your next cleaning for each new customer referral.</p>
+            </div>
+
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-12">
+            <div class="widget-box">
+                <h3 class="widget-head">$50 OFF Recurring Customers</h3>
+                <p class="widget-p">Take $25 OFF Your 2nd Recurring Cleaning. Take $25 OFF Your 4th Recurring Cleaning.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="contact-div">
+    <h6>*Mention these offers to the Maid Bright representative. Restrictions may apply.</h6>
+    <h3>Don’t delay - Start saving on your next Maid Bright cleaning service today!</h3>
+    <div class="button-div">
+        <button  type="button" class="default-button1"><a href="http://"></a>Contact Us</button>
     </div>
 </div>
 
