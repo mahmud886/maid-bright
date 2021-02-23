cd3f173eca4e7125adb550c6113860c315e8117a
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2021-02-23 14:29:06 +0600

    2 pages done aboutus pages

--- resources/views/pages/aboutus/meet-the-team.blade.php
+++ resources/views/pages/aboutus/meet-the-team.blade.php
@@ -6,6 +6,175 @@ Maid Bangladesh | About Us
 
 @section('content')
 
-<h1>Meet The Team Page</h1>
+<section class="banner-static why-maid1" style="background: url('/assets/images/banner/banner-image1.jpg');">
+    <div class="container">
+        <div class="banner-contents">
+            <div class="row">
+                <div class="col-50 banner-space banner-space2">
+                    <div class="banner-overlay-color">
+                        <div class="banner-contents-div">
+                            <h1><b>Meet Our Team</b></h1>
+                        </div>
+                        <p style="text-align: justify">Get to know the wonderful office staff who oversee Maid Bright’s
+                            operations and are responsible for our superior reputation and award-winning service.</p>
 
+                    </div>
+                </div>
+                <div class="col-50 banner-space banner-space3">
+                    <div class="spaces">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+<!-- < Management Team Start > -->
+<div class="container">
+    <div class="management-team p-4">
+        <h2 class="text-center">Management Team</h2>
+        <div class="row">
+            <div class="col">
+                <div class="management-team-div text-center">
+                    <div class="image-inner-content">
+                        <img src="/assets/images/aboutus/team/m-team1.jpg" alt="" srcset="">
+                        <h3 class="pt-4">Yusuf Mehmetoglu</h3>
+                        <p>CEO</p>
+                    </div>
+                    <div class="image-outer-content">
+                        <p>Yusuf is the co-founder and owner of Maid Bright, a residential cleaning
+                            service in the
+                            Washington, DC metro area. Started in 2004, Yusuf and his team have grown the business into
+                            a
+                            highly reputable, family-owned cleaning service.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="management-team-div  text-center">
+                    <div class="image-inner-content">
+                        <img src="/assets/images/aboutus/team/m-team2.jpg" alt="" srcset="">
+                        <h3 class="pt-4">Maria Castro</h3>
+                        <p>HR Manager</p>
+                    </div>
+                    <div class="image-outer-content">
+                        <p>Maria joined Maid Bright in 2012 as a Crew Coordinator. She has quickly advanced to a
+                            management position overseeing hiring, payroll, scheduling and customer relations.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="management-team-div text-center">
+                    <div class="image-inner-content ">
+                        <img src="/assets/images/aboutus/team/m-team3.jpg" alt="" srcset="">
+                        <h3 class="pt-4">Sep Sarbakhshian</h3>
+                        <p>Business Development</p>
+                    </div>
+                    <div class="image-outer-content">
+                        <p>Sep is a skilled communicator and problem solver addressing employee and customer needs. He
+                            is passionate about customer service and dedicated to providing the best experience for
+                            customers and team members alike.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- </ Management Team End > -->
+<!-- < Operation Team Start > -->
+<div class="container">
+    <div class="management-team p-4">
+        <h2 class="text-center">Management Team</h2>
+        <div class="row">
+            <div class="col-4">
+                <div class="management-team-div text-center">
+                    <div class="image-inner-content">
+                        <img src="/assets/images/aboutus/team/o-team1.jpg" alt="" srcset="">
+                        <h3 class="pt-4">Mahir DAG</h3>
+                        <p>Operations Manager</p>
+                    </div>
+                    <div class="image-outer-content">
+                        <p>Joining the team in 2016, Mahir leverages his customer relations skills to accommodate and
+                            help customers with their cleaning specifications. He takes customer concerns and feedback
+                            seriously, addressing them with the appropriate teams to create the best possible customer
+                            experience.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="management-team-div  text-center">
+                    <div class="image-inner-content">
+                        <img src="/assets/images/aboutus/team/o-team2.jpg" alt="" srcset="">
+                        <h3 class="pt-4">Estefany Lopez</h3>
+                        <p>Scheduling Manager</p>
+                    </div>
+                    <div class="image-outer-content">
+                        <p>Estefany is an adept listener addressing customer requests and inquiries with a professional
+                            attitude. She effectively manages the schedules of estimates and cleanings, making sure the
+                            team arrives promptly for all appointments.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="management-team-div text-center">
+                    <div class="image-inner-content ">
+                        <img src="/assets/images/aboutus/team/o-team3.jpg" alt="" srcset="">
+                        <h3 class="pt-4">Ahmed Ahmed</h3>
+                        <p>Sales manager</p>
+                    </div>
+                    <div class="image-outer-content">
+                        <p>With 15+ years experience in management and sales, Ahmed conducts free in-home consultations
+                            with potential customers. He pays attention to cleaning details and tailors the estimate
+                            based on specific needs. His outgoing personality is contagious making it a pleasure for all
+                            who interact with him.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="management-team-div text-center">
+                    <div class="image-inner-content ">
+                        <img src="/assets/images/aboutus/team/o-team4.jpg" alt="" srcset="">
+                        <h3 class="pt-4">Jose Villatoro</h3>
+                        <p>Maintenance Coordinator</p>
+                    </div>
+                    <div class="image-outer-content">
+                        <p>Jose takes great care of our company vehicles and equipment so everything is in working
+                            order. He coordinates the day-to-day logistics and operations paying close attention that
+                            company resources are used efficiently and effectively.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="management-team-div text-center">
+                    <div class="image-inner-content ">
+                        <img src="/assets/images/aboutus/team/o-team5.jpg" alt="" srcset="">
+                        <h3 class="pt-4">Kubra Dogan</h3>
+                        <p>Sales Manager</p>
+                    </div>
+                    <div class="image-outer-content">
+                        <p>If you call or email us, chances are Kubra will be the one responding. As our star
+                            administrator, Kubra dedicates her time to responding to customers offering informative and
+                            reliable answers with a professional demeanor.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-4">
+                <div class="management-team-div text-center">
+                    <div class="image-inner-content ">
+                        <img src="/assets/images/aboutus/team/o-team6.jpg" alt="" srcset="">
+                        <h3 class="pt-4">Idalia Bonilla</h3>
+                        <p>Customer Service</p>
+                    </div>
+                    <div class="image-outer-content">
+                        <p>The newest member of Maid Bright, Idalia prides herself in offering top-notch customer
+                            service. She answers phone calls and customer requests with grace and ease.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- </ Operation Team End > -->
 @endsection
--- resources/views/pages/aboutus/why-maid-bright.blade.php
+++ resources/views/pages/aboutus/why-maid-bright.blade.php
@@ -6,6 +6,108 @@ Maid Bangladesh | About Us
 
 @section('content')
 
-<h1>Why Maid Bright Page</h1>
+<section class="banner-static why-maid1" style="background: url('/assets/images/aboutus/whymaid1.jpg');">
+    <div class="container">
+        <div class="banner-contents">
+            <div class="row">
+                <div class="col-50 banner-space banner-space2">
+                    <div class="banner-overlay-color">
+                        <div class="banner-contents-div">
+                            <h1><b>Family-Owned and Trusted Since 2004</b></h1>
+                        </div>
+                        <p style="text-align: justify">For more than 17 years, Maid Bright has offered residential
+                            cleaning services in the Washington, DC metro area. Family owned and operated, we are a
+                            professional cleaning company specializing in a variety of services ensuring your home is
+                            cleaned to your satisfaction.</p>
+                        <div class="button-div">
+                            <button type="button" class="default-button1"><a href="http://"></a>Schedule A Free
+                                Estimate</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-50 banner-space banner-space3">
+                    <div class="spaces">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="why-maid-container">
+    <div class="why-maid-contents">
+        <div class="why-maid-first-content">
+            <h2 class="text-center">7 Reasons Why Maid Bright</h2>
+            <p>Our company philosophy lies in the fact that each and every customer is an essential part of our growing
+                family in Northern Virginia, DC and Maryland. We strive for excellence in each cleaning service that we
+                provide in order to establish strong family bonds. At Maid Bright, we care about the relationships we
+                continually build, which makes us a highly recommended and award winning home cleaning service.</p>
+        </div>
+        <div class="why-maid-second-content pt-4">
+            <div class="row">
+                <div class="col-3">
+                    <h2>Professional</h2>
+                </div>
+                <div class="col-9">
+                    <p>For your security and peace of mind, we do background checks on all employees. When
+                        your trained
+                        cleaning team arrives at your home, they come dressed in uniform with the equipment and cleaning
+                        supplies they need to get the job done.</p>
+                </div>
+                <div class="col-3">
+                    <h2>Prompt</h2>
+                </div>
+                <div class="col-9">
+                    <p>
+                        When you schedule a cleaning with Maid Bright, the team arrives on time every time. We are
+                        available to answer your questions or address concerns during business hours, Monday through
+                        Friday 8AM to 5PM and Saturday 8AM to 3PM.</p>
+                </div>
+                <div class="col-3">
+                    <h2>Go Beyond</h2>
+                </div>
+                <div class="col-9">
+                    <p>Unlike other cleaning services Maid Bright is detail-oriented, cleaning places you may not even
+                        expect. From cobwebs to baseboards, top of refrigerators to toilet seat hinges, we tackle these
+                        areas during every cleaning.</p>
+                </div>
+                <div class="col-3">
+                    <h2>Free Estimates</h2>
+                </div>
+                <div class="col-9">
+                    <p>Our pricing is competitive without compromising quality. A Maid Bright representative will assess
+                        your home, explain our service, and address all questions writing down notes and instructions on
+                        how to customize your cleaning. Once you receive your quote, you are not bound by a contract.
+                    </p>
+                </div>
+                <div class="col-3">
+                    <h2>Environmentally Safe Products</h2>
+                </div>
+                <div class="col-9">
+                    <p>Our cleaning supplies meet the industry’s Green Seal of Approval, safe for kids, pets and other
+                        family members. If there are alternative products you prefer, please provide at the time of your
+                        scheduled cleaning.</p>
+                </div>
+                <div class="col-3">
+                    <h2>Insured & Bonded</h2>
+                </div>
+                <div class="col-9">
+                    <p>Maid Bright has a business license and the proper paperwork protecting your belongings and
+                        ensuring we deliver the services agreed to.</p>
+                </div>
+                <div class="col-3">
+                    <h2>Guranteed</h2>
+                </div>
+                <div class="col-9">
+                    <p>If you are not 100% satisfied, please contact us within 24 hours of your cleaning. We will
+                        rectify the situation including cleaning the area in question at no additional charge. We
+                        follow-up with customers regularly to ensure our services are delivered to your satisfaction.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endsection
