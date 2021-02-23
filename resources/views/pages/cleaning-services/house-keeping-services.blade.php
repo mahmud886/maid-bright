85ba28ba0dcf02fbc3e218093b7ba87aca9331b5
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-02-23 15:41:11 +0600

    housekeeping page banner

--- resources/views/pages/cleaning-services/house-keeping-services.blade.php
+++ resources/views/pages/cleaning-services/house-keeping-services.blade.php
@@ -6,6 +6,29 @@ Maid Bangladesh | Cleaning Services
 
 @section('content')
 
-<h1>Page</h1>
+<section class="banner-static" style="background: url('/assets/images/banner/offers.jpg');">
+    <div class="container">
+        <div class="banner-contents">
+            <div class="row">
+                <div class="col-50 banner-space banner-space2">
+                    <div class="banner-overlay-color">
+                        <div class="banner-contents-div">
+                            <h1><b>A Cleaning Plan Tailored For You</b></h1>
+                        </div>
+                        <p style="text-align: center">Housekeeping services personalized for your specific needs.</p>
+                        <div class="button-div">
+                            <button  type="button" class="default-button1"><a href="http://"></a>Schedule A Free Estimate</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-50 banner-space banner-space3">
+                    <div class="spaces">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 @endsection
