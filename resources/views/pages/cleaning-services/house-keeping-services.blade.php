d3b338aae3dc4b2e7da3a472dec75f1607191fc7
Al-Amin Islam Hridoy <alaminislamhridoy@gmail.com>
2021-02-24 14:18:40 +0600

    house-keeping page

--- resources/views/pages/cleaning-services/house-keeping-services.blade.php
+++ resources/views/pages/cleaning-services/house-keeping-services.blade.php
@@ -30,5 +30,84 @@ Maid Bangladesh | Cleaning Services
         </div>
     </div>
 </section>
-
+<section class="">
+    <div class="mid-sec-extend">
+        <div class="mid-text">
+            <p class="text-p">Between work, family, and time for you, it is difficult to keep your space clean.  Maid Bright designed housekeeping services so you can spend more time doing the things you love.  Whether it is weekly, bi-weekly, monthly, or a one-time cleaning, we provide a flexible schedule to get the job done professionally.</p>
+        </div>
+        <div class="row cleaning-services text-works">
+            <div class="col25 col-xl-3 col-lg-3 col-md-6 col-sm-12 bg-light">
+                <h2>Vacation Rentals and Homes</h2>
+                <p>Get your vacation stay ready for guests or for yourself.</p>
+                
+            </div>
+            <div class="col25 col-xl-3 col-lg-3 col-md-6 col-sm-12 bg-light">
+                <h2>Rental Properties</h2>
+                <p>Schedule cleanings in-between tenants.</p>
+                
+            </div>
+            <div class="col25 col-xl-3 col-lg-3 col-md-6 col-sm-12 bg-light">
+                <h2>Airbnb’s</h2>
+                <p>Host guests in a space they will feel comfortable in and we will take care of the rest.</p>
+                
+            </div>
+            <div class="col25 col-xl-3 col-lg-3 col-md-6 col-sm-12 bg-light">
+                <h2>Apartments</h2>
+                <p>Not just for houses, Maid Bright specializes in cleaning smaller spaces too.</p>
+                
+            </div>
+            <div class="col25 col-xl-3 col-lg-3 col-md-6 col-sm-12 bg-light">
+                <h2>Special Cleaning</h2>
+                <p>Contact us for additional services such as oven cleaning, fridge cleaning, and more.</p>
+                
+            </div>
+        </div>
+    </div>
+</section>
+<section  class="bg-light">
+    <div class="mid-sec">
+        <div class="mid-text">
+            <h2 class="text-head">How It Works</h2>
+        </div>
+        <div class="row p-tb20">
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 text-works">
+                <span class="round-icon border border-primary">
+                    <i class="fa fa-home" aria-hidden="true"></i>
+                </span>
+                <h2>Ready and Uniformed</h2>
+                <p>The same cleaning crew will arrive at your home in uniform and equipped with environmentally friendly cleaning products</p>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 text-works">
+                <span class="round-icon border border-primary">
+                    <i class="fas fa-clipboard-check" aria-hidden="true"></i>
+                </span>
+                <h2>Adopt Your Plan</h2>
+                <p>Each team member will clean according to your customized cleaning checklist created for your home and needs</p>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 text-works">
+                <span class="round-icon border border-primary">
+                    <i class="fas fa-dollar-sign" aria-hidden="true"></i>
+                </span>
+                <h2>Follow-Up</h2>
+                <p>We consider our customers as part of our family and will periodically check-in to maintain our high quality standards.</p>
+            </div>
+        </div>
+        <p class="text-p">You may choose to be present at each cleaning or provide Maid Bright access to your home.  If you give us a key or garage code, it will be secured once received.</p>
+    </div>
+</section>
+<section>
+    <div class="mid-sec">
+        <div class="text-left">
+            <h2 class="text-head">The Benefits of Hiring Professional<br>Housekeeping Services</h2>
+            <p class="text-p">Whether you’re living in your home, renting it to guests, or trying to sell to prospective buyers, cleanliness is key. A fresh-smelling and immaculately maintained home offers immediate peace and refreshment. As a result, professional housekeeping and cleaning services will ensure that your guests are happy to stay — and return in the future. Proper cleaning can upgrade the quality and appeal of any home, from sleek modern apartments to rustic country cottages.</p>
+            <P class="text-p">Looking for housekeeping services for your own residence? After our thorough cleanings, you can breathe a deep sigh of relief when you walk in the door and feel the daily stresses fade. Let us handle the little details so that you can get back to enjoying the big picture of your life.</P>
+        </div>
+        <div class="text-left">
+            <h2 class="text-head">Why Turn to Maid Bright for Your<br>Housekeeping Cleaning Services?</h2>
+            <p class="text-p">At Maid Bright, no detail is too small to matter. We’re committed to leaving each home better and brighter than we found it so that our clients can enjoy the utmost relaxation. We can take the burden of housekeeping out of your hands and turn your home into a clean, sweet-smelling sanctuary for your family to enjoy.</p>
+            <P class="text-p">Our maid and housekeeping services are timely, professional, and dedicated to your complete satisfaction. Whether we have the pleasure of partnering with you for a one-time cleaning event or provide housekeeping services each week, you can count on the same incredible level of quality.</P>
+            <P class="text-p">For more than 17 years, we’ve set ourselves apart for clients across Virginia and Maryland. We’re known for being a cut above the competition and a go-to service for clients throughout the community. Here’s why.</P>
+        </div>
+    </div>
+</section>
 @endsection
