fb0b3d0370196cc6dd66366db71c93ee9b9514e2
Md. Iqbal Mahmud <61132992+mahmud886@users.noreply.github.com>
2021-02-27 17:46:55 +0600

    blog-section done

--- resources/views/pages/cleaning-services/seasonal-cleaning.blade.php
+++ resources/views/pages/cleaning-services/seasonal-cleaning.blade.php
@@ -6,6 +6,181 @@ Maid Bangladesh | Cleaning Services
 
 @section('content')
 
-<h1>Page</h1>
+
+<section>
+    <div class="row banner-static" style="background: url('/assets/images/banner/banner-image1.jpg');">
+        <div class="col-12">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-5 col-lg-5 col-md-11 col-sm-11 col-11">
+                        <div class="banner-contents">
+                            <div class="banner-overlay-color">
+                                <div class="banner-contents-div">
+                                    <h1><b>
+                                            Seasonal Cleaning and Family Gatherings</b></h1>
+                                </div>
+                                <p style="text-align: justify">Regular cleaning without the hassle. Schedule at your
+                                    convenience weekly, bi-weekly or monthly.</p>
+                                <div class="button-div">
+                                    <button type="button" class="default-button1"><a href="http://"></a>Schedule A Free
+                                        Estimate</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-7 col-lg-7 col-md-1 col-sm-1 col-1"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- < Recurring Contents Start > -->
+<div class="recurring-container pb-5">
+
+    <p class="text-justify pt-5">When the holidays come around, you likely find yourself with a lot on your mind — and
+        on your to-do list. Whether it’s Christmas, Easter, or Halloween, you want to have your house sparkle and shine
+        before guests start arriving. Yet between the decorating and cooking, who has time to clean? Take one big thing
+        off your plate by leaving your seasonal cleaning in our capable hands.</p>
+    <p class="pb-3">At Maid Bright, our one-time cleaning services will get your home ready for visitors before your
+        favorite holidays arrive.</p>
+    <div class="row">
+        <div class="col-4">
+            <div class="recurring-single-content text-center">
+                <h3>Weekly Cleaning Services</h3>
+                <p>A sought-after solution for busy executives and families who cannot dedicate time to clean their
+                    home.</p>
+            </div>
+        </div>
+        <div class="col-4">
+            <div class="recurring-single-content text-center">
+                <h3>Bi-Monthly Cleaning Services</h3>
+                <p>Ideal for those who do light cleaning every week or every other week but require a deeper clean for
+                    upkeep.</p>
+            </div>
+        </div>
+        <div class="col-4 mt-4">
+            <div class="recurring-single-content text-center">
+                <h3>Monthly Cleaning Services</h3>
+                <p>For people who clean their homes regularly, but could use a little help on occasion.</p>
+            </div>
+        </div>
+        <div class="recurring-paragraph">
+            <p>You may choose to be present at each cleaning or provide Maid Bright access to your home. If you give us
+                a key or garage code, it will be secured once received.</p>
+        </div>
+        <div class="recurring-tail-contents">
+            <h2>Holiday Cleaning Services</h2>
+            <p>Whether it’s a house full of visitors or a big holiday party, you want everything looking and smelling
+                its best. While a whole-home cleaning is the perfect way to showcase your house during the holidays,
+                it’s a big job. It takes hours of hard work and detail-oriented focus.</p>
+            <p>Our seasonal house cleaning services at Maid Bright can give your home the attention it needs to look its
+                best. We’ll leave you free to decorate cookies, baste a turkey, or complete other holiday duties before
+                your guests come calling.
+            </p>
+            <p>Our one-time house cleaning services will ensure that each space in your home is holiday-ready. Some of
+                the services and finishing touches we supply include:</p>
+            <ul class="main-container">
+                <li class="py-1">Dusted and polished areas that make the perfect party spaces</li>
+                <li class="py-1">Washed and sanitized kitchens for a pristine environment where you can cook your
+                    favorite seasonal dishes</li>
+                <li class="py-1">Cleaned and disinfected bathrooms that you’ll be proud to have guests see and use</li>
+                <li class="py-1">Fresh bedrooms that are thoroughly dusted and vacuumed for out-of-town guests</li>
+            </ul>
+
+            <div class="recurring-tail-title ">
+                <h2>The Maid Bright Difference</h2>
+            </div>
+
+            <p>Maid Bright is a family-owned and -operated cleaning company dedicated to providing the highest level of
+                detail-oriented service. From the moment you reach out to us, you’ll notice the difference. We ensure
+                prompt communication and stellar customer service every step of the way. We want to do more than clean
+                your oven or wash your windows — we want to create a lasting partnership as your go-to cleaning service.
+            </p>
+            <p>Here are just a few of the benefits you’ll experience when you choose us to handle your special house
+                cleanings:</p>
+            <ul class="main-container py-2">
+                <li class="py-2"> <b>Free estimates:</b> You won’t need to worry about any hidden costs with Maid
+                    Bright. A member of our team will come to your home to ensure that we provide you with an accurate,
+                    no-obligation quote for your special cleaning job.</li>
+                <li class="py-2"> <b> Customization: </b>Our flexible services will allow you to pay for the work you
+                    want done and nothing more.</li>
+                <li class="py-2"> <b>Reliability: </b> When we say we’ll be there, you can count on our word. We
+                    understand that your time is valuable.</li>
+                <li class="py-2"> <b>Safe and effective cleaning:</b> Now more than ever, you need cleaning products
+                    that ensure a deep, disinfecting clean. Our products are safe for home use yet highly effective.
+                </li>
+                <li class="py-2"> <b>Security:</b> Whether you choose to stick around or instead provide us with a key
+                    or garage code, you can rest assured that your property will be safe and secure. Our team is
+                    licensed, insured, and bonded, and your security and satisfaction are our top concerns.
+                </li>
+            </ul>
+
+
+            <h3>Learn More About Our Additional Special Cleaning Services</h3>
+            <p>Whether you need to enhance your cleaning routine or you want to take advantage of our sharp eye for
+                detail, reach out to our local cleaning company to learn more about our special services. You can also
+                request your free quote online to get started right away.</p>
+            <div class="recurring-footer p-2">
+                <h3 class="text-center">Interested in house cleaning services for your home? <br>
+                    Make an appointment for a free in-home estimate today.</h3>
+                <div class="row">
+                    <div class="col-3"></div>
+                    <div class="col-6">
+                        <div class="button-div">
+                            <button type="button" class="default-button1"><a href="http://"></a>Schedule Now</button>
+                        </div>
+                    </div>
+                    <div class="col-3"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="recurring-footer p-5 bg-light">
+    <div class="mid-text ">
+        <h2 class="text-head">How It Works</h2>
+    </div>
+    <div class="recurring-container">
+        <div class="row">
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 text-works">
+                <span class="round-icon border border-primary">
+                    <i class="fa fa-home" aria-hidden="true"></i>
+                </span>
+                <h2>In-Home Estimate</h2>
+                <p>After you schedule your free in-home consultation, a Maid Bright manager will come to your home and
+                    provide a personalized estimate.</p>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 text-works">
+                <span class="round-icon border border-primary">
+                    <i class="fas fa-clipboard-check" aria-hidden="true"></i>
+                </span>
+                <h2>Review Official Paperwork</h2>
+                <p>During the evaluation, you will be presented proof of Maid Bright’s certificate of insurance, bonding
+                    and references.</p>
+            </div>
+            <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12 text-works">
+                <span class="round-icon border border-primary">
+                    <i class="fas fa-dollar-sign" aria-hidden="true"></i>
+                </span>
+                <h2>Deliver Your Cost</h2>
+                <p>After the consultation, the Maid Bright manager will share a total cleaning cost based on home size,
+                    location,condition and frequency of service.</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="recurring-container">
+    <div class="row">
+        <div class="col-xl-8 col-lg-8 col-md-12 col-sm-12 img-group">
+            <img src="{{ asset('/assets/images/services/service6.jpg') }}" alt="">
+        </div>
+        <div class="col-xl-4 col-lg-4 col-md-12 col-sm-12 img-group">
+            <img src="{{ asset('/assets/images/services/service5.jpg') }}" alt="">
+            <img src="{{ asset('/assets/images/services/service7.jpg') }}" alt="">
+        </div>
+    </div>
+</div>
 
 @endsection
